53be1598d16e3f626e07337dbbeb5f3e3382e5e0
Alex Carpenedo <alex_carpenedo@hotmail.com>
2019-11-23 11:10:56 -0300

    Alterando compra

--- src/app/compra-page/dialogo-compra/dialogo-compra.component.ts
+++ src/app/compra-page/dialogo-compra/dialogo-compra.component.ts
@@ -1,44 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { Compra } from 'src/app/model/compra.model';
-import { CompraService } from 'src/app/services/compra.service';
-import { NotificacaoService } from 'src/app/services/notificacao.service';
 import { MatDialogRef } from '@angular/material';
+import { NotificacaoService } from './../../services/notificacao.service';
+import { CarrinhoService } from './../../services/carrinho.service';
+import { CompraService } from './../../services/compra.service';
+import { Compra } from './../../model/compra.model';
+import { Carrinho } from './../../model/carrinho.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dialogo-compra',
   templateUrl: './dialogo-compra.component.html',
   styleUrls: ['./dialogo-compra.component.css']
 })
-export class DialogoCompraComponent {
-  
-    compra: Compra = new Compra();
-  
-    constructor(
-      private compraService: CompraService,
-      private notificacaoService: NotificacaoService,
-      private dialogoReferencia: MatDialogRef<DialogoCompraComponent>
-  
-    ){}
-  
-    cancelar(){
-      this.dialogoReferencia.close();
-  
-    }
-  
-    salvar(){
-      this.compraService.salvar(this.compra).subscribe(res =>
-        {
-          this.dialogoReferencia.close(res);
-  
-        }, () => {
-          this.notificacaoService.mostrarMensagem(
-            "Não foi possivel salvar a compra!",
-            "OK", 
-            3000
-          );
-  
-        }
-        
-        );
-    }
+export class DialogoCompraComponent implements OnInit {
+
+  compra: Compra = new Compra();
+  carrinhos: Carrinho[];
+
+  constructor(
+    private compraService: CompraService,
+    private carrinhoService: CarrinhoService,
+    private notificacaoService: NotificacaoService,
+    private dialogoReferencia: MatDialogRef<DialogoCompraComponent>) {
+
   }
+
+  cancelar() {
+    this.dialogoReferencia.close();
+  }
+
+  salvar() {
+    this.compraService.salvar(this.compra).subscribe(
+      dados => {
+        this.dialogoReferencia.close(dados);
+      },
+      error => {
+        console.error(error);
+        this.notificacaoService.mostrarMensagem('Não foi possível salvar a compra!', 'OK', 3000);
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.listarCarrinhos();
+  }
+
+  listarCarrinhos() {
+    this.carrinhoService.listarCarrinhos().subscribe(
+      dados => {
+        if (dados.length === 0) {
+          this.notificacaoService.mostrarMensagem('A lista de carrinhos está vazia!', 'OK', 3000);
+        } else {
+          this.carrinhos = dados;
+        }
+      },
+      error => {
+        this.notificacaoService.mostrarMensagem('Não foi possível listar os carrinhos!', 'OK', 3000);
+        console.error(error);
+      }
+    );
+  }
+
+}
--- src/app/model/compra.model.ts
+++ src/app/model/compra.model.ts
@@ -2,7 +2,7 @@ import { Carrinho } from './carrinho.model';
 
 export class Compra {
     codigo : number;
-	carrinho : Carrinho;
+	compraCarrinho : number;
 	desconto : number;
 	valorcomdesconto : number;
 	dt_compra : Date;
