ca6ce30c9ecd5cabcbffec063bd4ac6aefe5b392
xxsilvoxx <37382434+xxsilvoxx@users.noreply.github.com>
2019-11-25 16:03:52 -0300

    pagamento alterado
    alterado o componente do pagamento page.html para usar o seletor de tipo de pagamento

---
+++ BackEnd/springrest/bin/src/main/resources/db/migration/V13__alterar_coluna_quantidade_da_tabela_carrinho_de_float_para_integer.sql
@@ -0,0 +1,2 @@
+ALTER TABLE carrinho DROP COLUMN quantidade;
+ALTER TABLE carrinho ADD quantidade integer;
---
+++ BackEnd/springrest/bin/src/main/resources/db/migration/V15__removendo_constraint_fk_de_cod_prod_car_e_removendo_colunas_cod_prod_car_e_sessao_da_tabela_carrinho.sql
@@ -0,0 +1,3 @@
+ALTER TABLE carrinho DROP FOREIGN KEY fk_produto_carrinho;
+ALTER TABLE carrinho DROP COLUMN cod_prod_car;
+ALTER TABLE carrinho DROP COLUMN sessao;
